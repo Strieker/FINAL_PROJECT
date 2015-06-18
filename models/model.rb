09e141e7e3172f2ce0f2b8ffbb13786c5214fdbf
Strieker <sagea17@mhs-la.org>
2015-06-18 21:25:41 +0000

    OMG

--- models/model.rb
+++ models/model.rb
@@ -2,6 +2,38 @@ class Result
   attr_accessor :input
   def response(input)
     @input=input
-    return @input.upcase
+    decoder={
+      :a.to_s=>"i",
+      :b.to_s=>"j",
+      :c.to_s=>"k",
+      :d.to_s=>"l",
+      :e.to_s=>"m",
+      :f.to_s=>"n",
+      :g.to_s=>"o",
+      :h.to_s=>"p",
+      :i.to_s=>"q",
+      :j.to_s=>"r",
+      :k.to_s=>"s",
+      :l.to_s=>"t",
+      :m.to_s=>"u",
+      :n.to_s=>"v",
+      :o.to_s=>"w",
+      :p.to_s=>"x",
+      :q.to_s=>"y",
+      :r.to_s=>"z",
+      :s.to_s=>"a",
+      :t.to_s=>"b",
+      :u.to_s=>"c",
+      :v.to_s=>"d",
+      :w.to_s=>"e",
+      :x.to_s=>"f",
+      :y.to_s=>"g",
+      :z.to_s=>"h"
+        }
+    @input= @input.split("")
+    @input.collect do |letter|
+      decoder[letter]
+    end
+      
   end
 end
